cc7c08e8a72b1abdf64ac89bdc446ada95fd9fde
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-24 02:51:47 +0530

    setup tamagui

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { TamaguiProvider, View } from '@tamagui/core';
+import { Button } from 'tamagui';
+
+import { tamaguiConfig } from './tamagui.config';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // hide splash screen here
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <TamaguiProvider config={tamaguiConfig}>
+      <View backgroundColor='red' />
+      <Button>asas</Button>
+    </TamaguiProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
