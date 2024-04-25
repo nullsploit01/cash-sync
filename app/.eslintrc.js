5bc6a51586ea51d8a44384fe36809a99a120a77d
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-26 01:50:19 +0530

    add eslint and prettier

---
+++ app/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    extends: [
+      'universe',
+      'universe/shared/typescript-analysis',
+      'plugin:react-hooks/recommended',
+      'plugin:testing-library/react',
+      'plugin:jest-dom/recommended'
+    ],
+    plugins: ['jest', 'testing-library', 'unused-imports'],
+    overrides: [
+      {
+        files: ['*.ts', '*.tsx', '*.d.ts'],
+        parserOptions: {
+          project: './tsconfig.json'
+        }
+      }
+    ],
+    settings: {
+      'import/resolver': {
+        typescript: {} // this loads <rootdir>/tsconfig.json to ESLint
+      }
+    },
+    /* for lint-staged */
+    globals: {
+      __dirname: true
+    },
+    rules: {
+      'no-console': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'no-unused-vars': 'off'
+    }
+  }
---
+++ app/src/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from 'tamagui';
+
+const HomePage = () => {
+  return (
+    <View
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}
+    >
+      <Text>Sup Brooo</Text>
+    </View>
+  );
+};
+
+export default HomePage;
