efb9023f50bf070144064abc3d9b4195c0e88994
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-27 20:58:44 +0530

    create a basic component to show net balance etc

--- app/src/app/index.tsx
+++ app/src/app/index.tsx
@@ -1,17 +1,21 @@
-import { Text, View } from 'tamagui'
+import { Card, Separator, SizableText, XStack, YStack } from 'tamagui'
 
 const HomePage = () => {
   return (
-    <View
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%'
-      }}
-    >
-      <Text>Sup Brooo</Text>
-    </View>
+    <XStack padding="$3" alignSelf="center">
+      <Card elevate width="100%" style={{ marginTop: 10 }} bordered>
+        <Card.Header padded>
+          <SizableText size="$7" fontWeight="700">
+            Net Balance
+          </SizableText>
+          <Separator marginTop={5} />
+          <YStack style={{ paddingTop: 10 }} gap={8}>
+            <SizableText size="$5">Net In</SizableText>
+            <SizableText size="$5">Net Out</SizableText>
+          </YStack>
+        </Card.Header>
+      </Card>
+    </XStack>
   )
 }
 
