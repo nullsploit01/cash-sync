00415308ef9e3fc20e409f1f4b4e0d7d0fc0a321
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-28 17:55:47 +0530

    reset softwareKeyboardLayoutMode

--- app/src/app/add-entry/index.tsx
+++ app/src/app/add-entry/index.tsx
@@ -1,6 +1,6 @@
 import { CalendarDays, ChevronDown, Clock4 } from '@tamagui/lucide-icons'
 import { Stack } from 'expo-router'
-import { Text, View } from 'tamagui'
+import { Input, Text, View } from 'tamagui'
 
 import Layout from 'src/components/layout'
 import useDatePicker from 'src/hooks/use-date-picker'
@@ -10,28 +10,38 @@ const AddEntryPage = () => {
   const { date, showDatepicker } = useDatePicker()
 
   return (
-    <Layout>
+    <Layout showEntryButtons>
       <Stack.Screen options={{ title: 'Add Entry' }} />
-      <View display="flex" flexDirection="row" justifyContent="space-between" padding="$3">
+      <View paddingHorizontal="$3">
         <View
-          onPress={() => showDatepicker()}
           display="flex"
           flexDirection="row"
-          alignItems="center"
+          justifyContent="space-between"
+          paddingVertical="$3"
         >
-          <CalendarDays size={16} strokeWidth="$0.25" />
-          <Text marginHorizontal="$1.5">{getFormattedDate(date)}</Text>
-          <ChevronDown size={16} strokeWidth="$0.5" />
+          <View
+            onPress={() => showDatepicker()}
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+          >
+            <CalendarDays size={16} strokeWidth="$0.25" />
+            <Text marginHorizontal="$1.5">{getFormattedDate(date)}</Text>
+            <ChevronDown size={16} strokeWidth="$0.5" />
+          </View>
+          <View
+            onPress={() => showDatepicker('time')}
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+          >
+            <Clock4 size={16} strokeWidth="$0.25" />
+            <Text marginHorizontal="$1.5">{getFormattedTime(date)}</Text>
+            <ChevronDown size={16} strokeWidth="$0.5" />
+          </View>
         </View>
-        <View
-          onPress={() => showDatepicker('time')}
-          display="flex"
-          flexDirection="row"
-          alignItems="center"
-        >
-          <Clock4 size={16} strokeWidth="$0.25" />
-          <Text marginHorizontal="$1.5">{getFormattedTime(date)}</Text>
-          <ChevronDown size={16} strokeWidth="$0.5" />
+        <View paddingVertical="$3">
+          <Input autoFocus placeholder="Amount" borderColor="black" size="$5" />
         </View>
       </View>
     </Layout>
