cccfd98a747b52a09d3ac7d39268935396f1e369
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-08 03:16:47 +0530

    add a middleware to protect routes

--- server/router/router.go
+++ server/router/router.go
@@ -2,7 +2,9 @@ package router
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/clerk/clerk-sdk-go/v2/jwt"
 	"github.com/gin-gonic/gin"
 	"github.com/nullsploit01/cash-sync/pkg/settings"
 	"github.com/nullsploit01/cash-sync/router/api"
@@ -14,13 +16,37 @@ func pong(c *gin.Context) {
 
 func InitRouter() *gin.Engine {
 	router := gin.Default()
+
 	router.Use(gin.Recovery())
 	router.Use(gin.Logger())
 
 	router.GET("/ping", pong)
 
-	router.GET("/auth/users/:id", api.GetUser)
-	router.PUT("/auth/users/:id", api.UpdateUser)
+	authRouter := router.Group("/auth")
+
+	authRouter.Use(ProtectedRoute())
+	{
+		authRouter.GET("/users/:id", api.GetUser)
+		authRouter.PUT("/users/:id", api.UpdateUser)
+	}
 
 	return router
+}
+
+func ProtectedRoute() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		sessionToken := strings.TrimPrefix(c.Request.Header.Get("Authorization"), "Bearer ")
+
+		// Verify the session
+		_, err := jwt.Verify(c.Request.Context(), &jwt.VerifyParams{
+			Token: sessionToken,
+		})
+
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			return
+		}
+
+		c.Next()
+	}
 }
