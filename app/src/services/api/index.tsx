4cfa7cb32ee87a08535e046218c792b9b4ec5096
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-11 21:05:24 +0530

    add ability to show http errors

--- app/src/services/api/index.tsx
+++ app/src/services/api/index.tsx
@@ -1,8 +1,9 @@
 import { useSession } from '@clerk/clerk-expo'
-import axios, { InternalAxiosRequestConfig } from 'axios'
+import axios, { AxiosError, AxiosResponse, HttpStatusCode, InternalAxiosRequestConfig } from 'axios'
 import { FC, PropsWithChildren, useEffect, useState } from 'react'
 
 import { Environment } from 'src/config/environment'
+import { useNotification } from 'src/hooks/use-notification'
 
 export const httpClient = axios.create({
   baseURL: Environment.API_URL
@@ -10,7 +11,13 @@ export const httpClient = axios.create({
 
 export const HttpInterceptor: FC<PropsWithChildren> = ({ children }) => {
   const { session, isLoaded } = useSession()
+  const { showNotification } = useNotification()
+
   const [isMounted, setIsMounted] = useState(false)
+
+  const getDescription = (data: any) => {
+    return typeof data?.error === 'string' ? data.error : 'Something went wrong! Please try again'
+  }
 
   useEffect(() => {
     const requestInterceptor = async (request: InternalAxiosRequestConfig) => {
@@ -25,12 +32,63 @@ export const HttpInterceptor: FC<PropsWithChildren> = ({ children }) => {
       return request
     }
 
-    const interceptor = httpClient.interceptors.request.use(requestInterceptor)
+    const responseInterceptor = (response: AxiosResponse) => {
+      return response
+    }
+
+    const errorInterceptor = (error: AxiosError) => {
+      switch (error.response?.status) {
+        case HttpStatusCode.BadRequest: {
+          showNotification({
+            title: 'Bad Request',
+            message: getDescription(error.response?.data),
+            type: 'error'
+          })
+          break
+        }
+
+        case HttpStatusCode.NotFound: {
+          showNotification({
+            title: 'Not Found',
+            message: getDescription(error.response?.data),
+            type: 'error'
+          })
+          break
+        }
+
+        case HttpStatusCode.InternalServerError: {
+          showNotification({
+            title: 'Internal Server Error',
+            message: getDescription(error.response?.data),
+            type: 'error'
+          })
+          break
+        }
+
+        default: {
+          showNotification({
+            title: 'Oops?',
+            message: getDescription(error.response?.data),
+            type: 'error'
+          })
+          break
+        }
+      }
+
+      return Promise.reject(error)
+    }
+
+    const requestInterceptorInstance = httpClient.interceptors.request.use(requestInterceptor)
+    const responseInterceptorInstance = httpClient.interceptors.response.use(
+      responseInterceptor,
+      errorInterceptor
+    )
 
     setIsMounted(true)
 
     return () => {
-      httpClient.interceptors.request.eject(interceptor)
+      httpClient.interceptors.request.eject(requestInterceptorInstance)
+      httpClient.interceptors.response.eject(responseInterceptorInstance)
     }
   }, [isMounted, session])
 
