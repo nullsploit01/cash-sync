4814a70b82768ac99580c7410c4d5c8470a2436b
nullsploit01 <harshaldharmik099@gmail.com>
2024-05-12 23:36:06 +0530

    add request validation for AddBook call

--- server/router/api/book.go
+++ server/router/api/book.go
@@ -7,18 +7,26 @@ import (
 	"github.com/nullsploit01/cash-sync/pkg/errors"
 )
 
+type AddBookRequest struct {
+	Name string `json:"name" binding:"required"`
+}
+
 func GetBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "get books"})
 }
 
 func AddBook(c *gin.Context) {
-	user, exists := c.Get("user")
+	// user, exists := c.Get("user")
 
-	if !exists {
-		c.Error(errors.Unauthorized())
+	var requestBody AddBookRequest
+
+	err := c.ShouldBindJSON(&requestBody)
+
+	if err != nil {
+		c.Error(errors.BadRequest(err.Error()))
 		c.Abort()
 		return
 	}
 
-	c.IndentedJSON(http.StatusCreated, gin.H{"message": user})
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": requestBody})
 }
